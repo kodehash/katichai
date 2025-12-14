7665821b2f9a1459112b24bbb09e6a59513e1761
Ko <kousick.shanmugam@gmail.com>
2025-12-14 14:36:51 +0530

    Add duplicate test file

---
+++ internal/test_dup.go
@@ -0,0 +1,27 @@
+package internal
+
+import "math"
+
+// CosineSimilarity calculates cosine similarity between two vectors
+// Copied from embeddings/similarity.go for testing
+func cosineSimilarity(a, b []float32) float32 {
+	if len(a) != len(b) {
+		return 0
+	}
+
+	var dotProduct float64
+	var normA float64
+	var normB float64
+
+	for i := 0; i < len(a); i++ {
+		dotProduct += float64(a[i]) * float64(b[i])
+		normA += float64(a[i]) * float64(a[i])
+		normB += float64(b[i]) * float64(b[i])
+	}
+
+	if normA == 0 || normB == 0 {
+		return 0
+	}
+
+	return float32(dotProduct / (math.Sqrt(normA) * math.Sqrt(normB)))
+}
